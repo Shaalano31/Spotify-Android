e6ea0004eb71484efe7e822ee8cc29cf6dcb792b
Khaled Ali Shaalan <khaled.shaalan.031@gmail.com>
2020-03-12 19:25:12 +0200

    Sign in with facebook button added

--- app/src/androidTest/java/com/example/spotify/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/spotify/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.spotify;
 
 import android.content.Context;
 
---
+++ app/src/main/java/com/example/spotify/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.spotify;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+
+    private static final String EMAIL = "email";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void LogIn (View view) {
+
+    }
+
+    public void SignUp (View view) {
+
+    }
+}
