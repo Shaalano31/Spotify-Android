2b03ad039c56b5f64df3b7c9a3ff6ed2f79e6d9e
Khaled Ali Shaalan <khaled.shaalan.031@gmail.com>
2020-06-07 15:45:16 +0200

    Added a class to handle basic artist information

--- app/src/main/java/com/example/spotify/Users.java
+++ app/src/main/java/com/example/spotify/Users.java
@@ -4,143 +4,65 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Users {
+
     @SerializedName("userName")
     @Expose
     private String userName;
+
     @SerializedName("email")
     @Expose
     private String email;
+
     @SerializedName("password")
     @Expose
     private String password;
-    @SerializedName("isPremium")
-    @Expose
-    private boolean isPremium;
-    @SerializedName("isActive")
-    @Expose
-    private boolean isActive;
-
-
-    @SerializedName("day")
-    @Expose
-    private String day;
-
-    @SerializedName("month")
-    @Expose
-    private String month;
-
-    @SerializedName("year")
-    @Expose
-    private String year;
-
-    /*@SerializedName("birthDate")
-    @Expose
-    private String birthDate;*/
-
-
 
     @SerializedName("gender")
     @Expose
     private String gender;
-    @SerializedName("body")
+
+    @SerializedName("dateOfBirth")
     @Expose
-    private String text;
+    private String dateOfBirth;
 
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    //"_id": "5e8a799494a3760aef918874",
-    //            "userName": "Khaled",
-    //            "imagePath": "defaultuser.png"
-
-    @SerializedName("_id")
-    @Expose
-    private String id;
-
-    @SerializedName("imagePath")
-    @Expose
-    private String imagePath;
-
-
-    public Users(String username, String email, String password, boolean b, boolean b1, String day, String month, String year, String gender) {
-    }
-    public Users() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getUserName() {
         return userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public boolean isPremium() {
-        return isPremium;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    /*public String getBirthDate() {
-        return birthDate;
-    }*/
-
-    public String getGender() {
-        return gender;
     }
 
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public String getImagePath() {
-        return imagePath;
+    public String getEmail() {
+        return email;
     }
 
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getDay() {
-        return day;
+    public String getPassword() {
+        return password;
     }
 
-    public void setDay(String day) {
-        this.day = day;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getMonth() {
-        return month;
+    public String getGender() {
+        return gender;
     }
 
-    public void setMonth(String month) {
-        this.month = month;
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
-    public String getYear() {
-        return year;
+    public String getDateOfBirth() {
+        return dateOfBirth;
     }
 
-    public void setYear(String year) {
-        this.year = year;
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 }
