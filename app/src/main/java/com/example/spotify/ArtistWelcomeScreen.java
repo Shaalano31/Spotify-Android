46ed062c24074d3817a9403b80a2b6f4b4bb8383
Khaled Ali Shaalan <khaled.shaalan.031@gmail.com>
2020-06-11 20:59:46 +0200

    Imrpoved some UI/UX

--- app/src/main/java/com/example/spotify/ArtistWelcomeScreen.java
+++ app/src/main/java/com/example/spotify/ArtistWelcomeScreen.java
@@ -2,8 +2,11 @@ package com.example.spotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ArtistWelcomeScreen extends AppCompatActivity {
@@ -14,8 +17,32 @@ public class ArtistWelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_artist_welcome_screen);
     }
 
-    public void Testing(View view) {
+    public void AddSong(View view) {
         Intent intent = new Intent(getApplicationContext(), ArtistUploadMenu.class);
         startActivity(intent);
+    }
+
+    public void logOut(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public  void addAlbum(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Error");
+        alert.setMessage("Functionality under fixing, please use 'Add Song'");
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alert.create().show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.i("Info", "Back pressed");
+        moveTaskToBack(true);
     }
 }
